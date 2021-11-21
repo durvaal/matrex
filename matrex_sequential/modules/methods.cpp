5736a261b414fed39ba56ee9dffcf8afbce7b425
Paulo Lima <paulo.lima@zrp.com.br>
2021-11-21 10:59:08 -0300

    chore handle matrices with parallel thread

--- matrex_sequential/modules/methods.cpp
+++ matrex_sequential/modules/methods.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <random>
 #include <chrono>
+#include <pthread.h>
 #include "./headers/matrix_class.h"
 #include "./headers/methods.h"
 
@@ -90,8 +91,7 @@ Matrix *loadMatrix(string file) {
   return matrix;
 }
 
-void writerFileWithMatricesMultiplication(Matrix *matricesProduct, int calculationTimeInNanoSeconds)
-{
+void writerFileWithMatricesMultiplication(Matrix *matricesProduct, int calculationTimeInNanoSeconds){
   cout << "Init writerFileWithMatricesMultiplication \n";
 
   ofstream matricesProductFile;
@@ -112,14 +112,14 @@ void writerFileWithMatricesMultiplication(Matrix *matricesProduct, int calculati
   cout << "End writerFileWithMatricesMultiplication \n";
 };
 
-void calculateMatricesMultiplication(Matrix *matrix1, Matrix *matrix2) {
-  cout << "Init calculateMatricesMultiplication \n";
+void calculateMatricesMultiplicationSequentially(Matrix *matrix1, Matrix *matrix2) {
+  cout << "Init calculateMatricesMultiplicationSequentially \n";
 
   if (matrix1->getCol() == matrix2->getRow()) {
     Matrix *matricesProduct = new Matrix();
 
     matricesProduct->setRow(matrix1->getRow());
-    matricesProduct->setCol(matrix1->getCol());
+    matricesProduct->setCol(matrix2->getCol());
 
     for (int row = 0; row < matrix1->getRow(); row++) {
       vector<int> emptyVector;
@@ -159,14 +159,90 @@ void calculateMatricesMultiplication(Matrix *matrix1, Matrix *matrix2) {
     throwException("Runtime error: It's not possible to multiply the matrices, the matrix1 has " + to_string(matrix1->getCol()) + " columns and the matrix2 has " + to_string(matrix2->getRow()) + " rows");
   }
 
-  cout << "End calculateMatricesMultiplication \n";
+  cout << "End calculateMatricesMultiplicationSequentially \n";
 }
 
-void loadSystem() {
+void handleMatricesSequentially() {
   Matrix *matrix1 = loadMatrix("m1.txt");
   Matrix *matrix2 = loadMatrix("m2.txt");
 
-  calculateMatricesMultiplication(matrix1, matrix2);
+  calculateMatricesMultiplicationSequentially(matrix1, matrix2);
+
+  delete matrix1;
+  delete matrix2;
+}
+
+//Each thread computes single element in the resultant matrix
+void *mult(void* arg){
+  int *data = (int *)arg;
+  int k = 0, i = 0;
+    
+  int x = data[0];
+  
+  for (i = 1; i <= x; i++) {
+    k += data[i]*data[i+x];
+  }
+    
+  int *p = (int*)malloc(sizeof(int));
+  *p = k;
+     
+  //Used to terminate a thread and the return value is passed as a pointer
+  pthread_exit(p);
+}
+
+void calculateMatricesMultiplicationParallelThread(Matrix *matrix1, Matrix *matrix2) {
+  int r1 = matrix1->getRow(), c1 = matrix1->getCol(), r2 = matrix2->getRow(), c2 = matrix2->getCol(), i, j, k;
+
+  int max = r1 * c2;
+
+  //declaring array of threads of size r1*c2
+  pthread_t *threads;
+  threads = (pthread_t *)malloc(max * sizeof(pthread_t));
+
+  int count = 0;
+  int *data = NULL;
+  for (i = 0; i < r1; i++) {
+    for (j = 0; j < c2; j++) {
+      //storing row and column elements in data
+      data = (int *)malloc((20) * sizeof(int));
+      data[0] = c1;
+
+      for (k = 0; k < c1; k++) {
+        data[k + 1] = matrix1->getElementValue(i, k);
+      }
+
+      for (k = 0; k < r2; k++) {
+        data[k + c1 + 1] = matrix2->getElementValue(k, j);
+      }
+
+      //creating threads
+      pthread_create(&threads[count++], NULL, mult, (void *)(data));
+    }
+  }
+
+  cout << "RESULTANT MATRIX IS :- \n";
+
+  for (i = 0; i < max; i++) {
+    void *k;
+
+    //Joining all threads and collecting return value
+    pthread_join(threads[i], &k);
+
+    int *p = (int *)k;
+
+    cout << *p << " ";
+    
+    if ((i + 1) % c2 == 0) {
+      cout << "\n";
+    }
+  }
+}
+
+void handleMatricesWithParallelThread() {
+  Matrix *matrix1 = loadMatrix("m1.txt");
+  Matrix *matrix2 = loadMatrix("m2.txt");
+
+  calculateMatricesMultiplicationParallelThread(matrix1, matrix2);
 
   delete matrix1;
   delete matrix2;
@@ -230,15 +306,14 @@ void writerMatrix(){
 
   pArqMatriz1.close();
   pArqMatriz2.close();
-
-  loadSystem();
 }
 
 void listAvailableCommands() {
   cout << "\n:::: Available Commands ::::\n\n";
   cout << "quit                                           Exit the system\n";
   cout << "wm                                             Start write m1 and m2 matrices \n";
-  cout << "rsm                                            Start reading and calculating sequentially matrices \n";
+  cout << "rms                                            Start reading and calculating sequentially matrices \n";
+  cout << "rmpt                                           Start reading and calculating parallel thread matrices \n";
   cout << "\n";
 }
 
@@ -262,9 +337,12 @@ void initializeProgram() {
       } else if (strcmp(arguments, "wm") == 0) {
         cout << "\n";
         writerMatrix();
-      } else if (strcmp(arguments, "rsm") == 0) {
+      } else if (strcmp(arguments, "rms") == 0) {
         cout << "\n";
-        loadSystem();
+        handleMatricesSequentially();
+      } else if (strcmp(arguments, "rmpt") == 0) {
+        cout << "\n";
+        handleMatricesWithParallelThread();
       } else if (strcmp(arguments, "help") == 0) {
         listAvailableCommands();
       } else {
